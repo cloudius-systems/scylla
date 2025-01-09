7c8e542d473d279a5c3e079059c2bcacaa35ac9f
Emil Maskovsky <emil.maskovsky@scylladb.com>
2025-01-09 12:12:57 +0100

    raft: use adaptive number of voters
    The maximum number of voters is adaptive, determined by the number
    of DCs in the cluster:
    * if there are less than 5 DCs, there are max 5 voters
    * if there are more than 5 and less than 9 DCs, there number of voters
      is equal to the number of DCs
    * if there are more than 9 DCs, there are max 9 voters

--- service/topology_coordinator.cc
+++ service/topology_coordinator.cc
@@ -3476,8 +3476,8 @@ future<> run_topology_coordinator(
         endpoint_lifecycle_notifier& lifecycle_notifier,
         gms::feature_service& feature_service) {
 
-    group0_voter_registry::instance_ptr voter_registry =
-            group0_voter_registry::create(std::make_unique<group0_server_info_accessor>(topo_sm._topology), std::make_unique<group0_voter_client>(group0));
+    group0_voter_registry::instance_ptr voter_registry = group0_voter_registry::create(std::make_unique<group0_server_info_accessor>(topo_sm._topology),
+            std::make_unique<group0_voter_client>(group0), group0_voter_registry::MAX_VOTERS_ADAPTIVE);
 
     topology_coordinator coordinator{
             sys_dist_ks, gossiper, messaging, shared_tm,
