37b9de56d3b15d0a051d9413b2243d9a60eaf3e7
Ernest Zaslavsky <ernest.zaslavsky@scylladb.com>
2025-01-16 09:29:50 +0200

    scylla-sstable: Enable support for S3-stored sstables
    This update allows Scylla to work with sstables stored on AWS S3. Users can specify the fully qualified location of the sstable using the format: `s3://prefix/sstable_name`.

--- test/boost/s3_test.cc
+++ test/boost/s3_test.cc
@@ -50,21 +50,21 @@ using namespace std::string_view_literals;
 //   export AWS_DEFAULT_REGION="us-east-2"
 
 static shared_ptr<s3::client> make_proxy_client(semaphore& mem) {
-    s3::endpoint_config cfg = {
+    aws::s3::endpoint_config cfg = {
         .port = std::stoul(tests::getenv_safe("PROXY_S3_SERVER_PORT")),
         .use_https = false,
         .region = ::getenv("AWS_DEFAULT_REGION") ?: "local",
     };
-    return s3::client::make(tests::getenv_safe("PROXY_S3_SERVER_HOST"), make_lw_shared<s3::endpoint_config>(std::move(cfg)), mem);
+    return s3::client::make(tests::getenv_safe("PROXY_S3_SERVER_HOST"), make_lw_shared<aws::s3::endpoint_config>(std::move(cfg)), mem);
 }
 
 static shared_ptr<s3::client> make_minio_client(semaphore& mem) {
-    s3::endpoint_config cfg = {
+    aws::s3::endpoint_config cfg = {
         .port = std::stoul(tests::getenv_safe("S3_SERVER_PORT_FOR_TEST")),
         .use_https = ::getenv("AWS_DEFAULT_REGION") != nullptr,
         .region = ::getenv("AWS_DEFAULT_REGION") ?: "local",
     };
-    return s3::client::make(tests::getenv_safe("S3_SERVER_ADDRESS_FOR_TEST"), make_lw_shared<s3::endpoint_config>(std::move(cfg)), mem);
+    return s3::client::make(tests::getenv_safe("S3_SERVER_ADDRESS_FOR_TEST"), make_lw_shared<aws::s3::endpoint_config>(std::move(cfg)), mem);
 }
 
 using client_maker_function = std::function<shared_ptr<s3::client>(semaphore&)>;
