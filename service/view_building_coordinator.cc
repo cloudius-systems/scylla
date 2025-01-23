a86028a7bcb466649fd5aa81f545b8dfc5244cbc
Michał Jadwiszczak <michal.jadwiszczak@scylladb.com>
2025-01-23 17:19:04 +0100

    service/vb_coordinator: add currently processing base
    Select first available base table as currently processing base.
    Save this information to `system.scylla_local`.

--- service/view_building_coordinator.cc
+++ service/view_building_coordinator.cc
@@ -37,6 +37,7 @@ namespace vbc {
 
 struct vbc_state {
     vbc_tasks tasks;
+    std::optional<table_id> processing_base;
 };
 
 class view_building_coordinator : public migration_listener::only_view_notifications {
@@ -77,8 +78,11 @@ private:
 
     future<vbc_state> load_coordinator_state() {
         auto tasks = co_await _sys_ks.get_view_building_coordinator_tasks();
+        auto processing_base = co_await _sys_ks.get_vbc_processing_base();
+
         co_return vbc_state {
-            .tasks = std::move(tasks)
+            .tasks = std::move(tasks),
+            .processing_base = std::move(processing_base),
         };
     }
     
@@ -134,7 +138,19 @@ future<std::optional<vbc_state>> view_building_coordinator::update_coordinator_s
     } else if (auto to_remove = get_views_to_remove(state, views); !to_remove.empty()) {
         for (auto& view: views) {
             co_await remove_view(view, batch);
+
+            if (state.processing_base && *state.processing_base == get_base_id(view)) {
+                auto mut = co_await _sys_ks.make_vbc_delete_processing_base_mutation(batch.write_timestamp());
+                batch.add_mutation(std::move(mut));
+            }
         }
+    } else if (!state.processing_base && !state.tasks.empty()) {
+        // select base table to process
+        auto& base_id = state.tasks.cbegin()->first;
+        vbc_logger.info("Start building views for base table: {}", base_id);
+
+        auto mut = co_await _sys_ks.make_vbc_processing_base_mutation(batch.write_timestamp(), base_id);
+        batch.add_mutation(std::move(mut));
     }
 
     if (!batch.empty()) {
